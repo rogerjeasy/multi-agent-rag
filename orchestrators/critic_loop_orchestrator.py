ed034bb78804b63efc9c083ee34d596f23c371b9
DBaratin <dimitribaratin@gmail.com>
2025-12-16 18:18:21 +0100

    sync

---
+++ orchestrators/critic_loop_orchestrator.py
@@ -0,0 +1,7 @@
+
+
+# ADDING A CRITIC LOOP
+## 1) Generator to produce an initial draft answer based on the retrieved documents
+## 2) Implementation of an LLM prompt to check the draft for missing info, inaccuracies, hallucinations
+## 3) Process - if it passes, we return the answer, if not, we revise the answer based on the feedback and return the revised answer
+
